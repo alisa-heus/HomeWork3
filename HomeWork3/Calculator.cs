921c0a395f6bb6fcb03edac66170cbd0a560fc16
Admin <Admin@DESKTOP-1VCHF71>
2021-11-05 17:45:14 +0200

    Add project files.

---
+++ HomeWork3/Calculator.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork3
+{
+    class Calculator
+    {
+       public static double Task1(double A, double B)
+        {
+            double result;
+            if (A < B)
+            {
+                result = A + B;
+
+            }
+            else if (A == B)
+            {
+                result = A * B;
+            }
+            else
+            {
+                result = A - B;
+            }
+
+            return result;
+        }
+
+       public static string Task2(double X, double Y)
+        {
+            string quarter;
+
+            if (X > 0)
+            {
+                if (Y > 0)
+                {
+                    quarter = "first";
+                }
+                else
+                {
+                    quarter = "fourth";
+                }
+            }
+            else
+            {
+                if (Y > 0)
+                {
+                    quarter = "second";
+                }
+                else
+                {
+                    quarter = "third";
+                }
+            }
+            return quarter;
+        }
+
+       public static (int, int, int) Task3(int A, int B, int C)
+        {
+            if (A > B)
+            {
+                Swap(ref A, ref B);
+            }
+            if (A > C)
+            {
+                Swap(ref A, ref C);
+            }
+            if (B > C)
+            {
+                Swap(ref B, ref C);
+            }
+
+            return (A, B, C);
+        }
+
+       public static string Task4(double A, double B, double C)
+        {
+            double determinanta = B * B - 4 * A * C;
+            if (determinanta < 0)
+            {
+                return "No intersection";
+            }
+            else if (determinanta == 0)
+            {
+                double result = (-1) * B / (2 * A);
+                return result.ToString(); 
+            }
+            else
+            {
+                double result1 = ((-1) * B + Math.Sqrt(determinanta)) / (2 * A);
+                double result2 = ((-1) * B - Math.Sqrt(determinanta)) / (2 * A);
+                return result1.ToString()+ "," + result2.ToString(); 
+            }
+
+        }
+
+       public static string Task5(int A)
+        {
+            int tensInt = A / 10;
+            int onesInt = A % 10;
+            if (tensInt == 1)
+            {
+                return teenConverter(A);
+            }
+            else 
+            {
+                string ones = onesConverter(onesInt);
+                string tens = tensConverter(tensInt);
+                return tens + " " + ones;
+            }
+            
+
+            
+        }
+
+       private static string onesConverter(int tensInt)
+        {
+            string ones = "";
+            switch (tensInt)
+            {
+                case 0:
+                    ones = "";
+                    break;
+                case 1:
+                    ones = "one";
+                    break;
+                case 2:
+                    ones = "two";
+                    break;
+                case 3:
+                    ones = "three";
+                    break;
+                case 4:
+                    ones = "four";
+                    break;
+                case 5:
+                    ones = "five";
+                    break;
+                case 6:
+                    ones = "six";
+                    break;
+                case 7:
+                    ones = "seven";
+                    break;
+                case 8:
+                    ones = "eight";
+                    break;
+                case 9:
+                    ones = "nine";
+                    break;
+            }
+            return ones;
+        }
+
+       private static string tensConverter(int tensInt)
+        {
+            string tens = "";
+            switch (tensInt)
+            {
+                case 2:
+                    tens = "twenty";
+                    break;
+                case 3:
+                    tens = "thirty";
+                    break;
+                case 4:
+                    tens = "fourty";
+                    break;
+                case 5:
+                    tens = "fifty";
+                    break;
+                case 6:
+                    tens = "sixty";
+                    break;
+                case 7:
+                    tens = "seventy";
+                    break;
+                case 8:
+                    tens = "eighty";
+                    break;
+                case 9:
+                    tens = "ninety";
+                    break;
+            }
+            return tens;
+        }
+
+       private static string teenConverter(int number)
+        {
+            string teen ="";
+            switch (number)
+            {
+                case 10:
+                    teen = "ten";
+                    break;
+                case 11:
+                    teen = "eleven";
+                    break;
+                case 12:
+                    teen = "twelve";
+                    break;
+                case 13:
+                    teen = "thirteen";
+                    break;
+                case 14:
+                    teen = "fourteen";
+                    break;
+                case 15:
+                    teen = "fifteen";
+                    break;
+                case 16:
+                    teen = "sixteen";
+                    break;
+                case 17:
+                    teen = "seventeen";
+                    break;
+                case 18:
+                    teen = "eighteen";
+                    break;
+                case 19:
+                    teen = "nineteen";
+                    break;       
+            }
+            return teen;
+        }
+       private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ HomeWork3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using HomeWork3;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task1();
+            //Task2();
+            //Task3();
+            //Task4();
+            //Task5();
+            
+        }
+
+       static void Task1()
+        {
+            Console.WriteLine("Enter a number");
+            var A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            var B = Convert.ToDouble(Console.ReadLine());
+
+            double result = Calculator.Task1(A, B);
+            Console.WriteLine(result);
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Type number X");
+            double X = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Type number Y");
+            double Y = Convert.ToDouble(Console.ReadLine());
+
+            string result = Calculator.Task2(X, Y);
+            Console.WriteLine(result);
+        }
+
+        static void Task3()
+        {
+            Console.WriteLine("Type number A");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type number B");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type number C");
+            int C = Convert.ToInt32(Console.ReadLine());
+
+            (int X, int Y, int Z) = Calculator.Task3(A, B, C);
+            Console.WriteLine($"Ascending numbers are {X}, {Y} and {Z}");    
+        }
+
+        static void Task4()
+        {
+            Console.WriteLine("Type number A");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Type number B");
+            double B = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Type number C");
+            double C = Convert.ToDouble(Console.ReadLine());
+
+            string result = Calculator.Task4(A, B, C);
+            Console.WriteLine(result);
+        }
+
+        static void Task5()
+        {
+                Console.WriteLine("Type two-digit number");
+                int A = Convert.ToInt32(Console.ReadLine());
+
+                string result = Calculator.Task5(A);
+                Console.WriteLine(result);
+        }
+
+    }
+}
